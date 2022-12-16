789b227700e8f3e7142f4cd929995a5aedeee25e
wsm516 <112995519+wsm516@users.noreply.github.com>
2022-12-16 21:09:50 +0900

    Update Program.cs

--- exercise11_8_2/Program.cs
+++ exercise11_8_2/Program.cs
@@ -10,7 +10,7 @@ namespace exercise11_8_2
     {
         /// <summary> 
         /// 해당 애플리케이션의 주 진입점입니다. 
-        /// </summary>
+        /// </summary> 
         [STAThread]
         static void Main()
         {
