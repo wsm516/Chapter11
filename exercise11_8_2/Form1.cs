3a4425e524ad0601509d34b6004f24dc9b7b5153
wsm516 <112995519+wsm516@users.noreply.github.com>
2022-12-16 21:09:33 +0900

    Update Form1.cs

--- exercise11_8_2/Form1.cs
+++ exercise11_8_2/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
  
-namespace exercise11_8_2
+namespace exercise11_8_2 
 {
     public partial class Form1 : Form
     {
